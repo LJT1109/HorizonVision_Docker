1aee63b77b6dfde87f214b4feafc0739f67b573d
LJT1109 <105228706+LJT1109@users.noreply.github.com>
2023-08-26 07:00:45 +0800

    zx

--- web_editor_project/backend/horizonvision/settings.py
+++ web_editor_project/backend/horizonvision/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=4ob_41@-v+^-q4qg&v8)twd)r49hk!s^e*yvi@+ec#as_o2q)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
